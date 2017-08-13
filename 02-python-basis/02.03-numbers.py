dbc4426a7a994fd922108f6d89d64fc2fd832a62
UniqueXiao <xiaoxin1219_work@163.com>
2017-08-13 16:20:45 +0800

    Create 02.03-numbers.py

---
+++ 02-python-basis/02.03-numbers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+#整型 Integers
+
+#长整型 Long Integers
+
+#浮点数 Floating Point Numbers
+
+#复数 Complex Numbers
+
+#类型转换
+
+#其他表示 除10进制外的表示方式
+2进制，前面加0b修饰，后面使用数字0或1：0b101010
+8进制，前面加0或者0o修饰，后面使用数字0-7：067
+16进制，前面加0x修饰，后面使用数字0-9A-F：0xFF
+科学计数法：1e-6
